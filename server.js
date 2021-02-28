ec3c69711f5de60c2bf6b65ec834f46854e0c9c0
Nat Weerawan <nat.wrw@gmail.com>
2021-02-28 08:23:31 +0000

    Update server.js

--- server.js
+++ server.js
@@ -13,9 +13,11 @@ app.use(express.static('dist'))
 
 // app.use('/react', express.static('react-app/build'))
 // app.use(express.static('vue-app/dist'))
-app.use(basicAuth({
-    users: { 'admin': 'supersecret' }
-}))
+app.use(
+  basicAuth({
+    users: { admin: 'supersecret' },
+  })
+)
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -29,22 +31,24 @@ app.get('/api/version', (req, res) => {
   res.status(200).send(`${version}`)
 })
 
-app.get("/check", (req, res) => {
+app.get('/check', (req, res) => {
   let timer = setTimeout(() => {
     res.status(500).send('timeout')
   }, 4000)
 
-    const MQTT_HOST = process.env.MQTT_HOST
-    const MQTT_USERNAME = process.env.MQTT_USERNAME
-    const MQTT_PASSWORD = process.env.MQTT_PASSWORD
-    const options = {
+  const MQTT_HOST = process.env.MQTT_HOST
+  const MQTT_USERNAME = process.env.MQTT_USERNAME
+  const MQTT_PASSWORD = process.env.MQTT_PASSWORD
+  const options = {
     port: 1883,
     clientId: 'mqtt-hc' + Math.random(),
     username: MQTT_USERNAME,
     password: MQTT_PASSWORD,
   }
 
+  console.log(options, `mqtt://${MQTT_HOST}`)
   const client = mqtt.connect(`mqtt://${MQTT_HOST}`, options)
+
   client.on('message', (topic, msg) => {
     console.log(topic, msg)
     client.end()
@@ -54,17 +58,14 @@ app.get("/check", (req, res) => {
 
   client.on('connect', () => {
     console.log('connected')
-    client.subscribe("#")
+    client.subscribe('#')
   })
 
   client.on('error', (err) => {
     res.status(500).send(`FAILED: ${err}`)
     clearTimeout(timer)
   })
-
 })
-
-
 
 const paths = app._router.stack.filter((v) => v.route).map((v) => v.route.path)
 
